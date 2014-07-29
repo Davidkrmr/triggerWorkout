227a96baa90a901fa802885c8ac6b05e7b38ebf4
David K <davkramer@gmail.com>
2014-07-29 13:19:22 +0200

    save function and dclick. Without debouce

--- .build/atmega328/src/trigger_riot.cpp
+++ .build/atmega328/src/trigger_riot.cpp
@@ -2,8 +2,9 @@
 #include <EEPROM.h>
 int checkProb(uint8_t num, uint8_t comp);
 void eepromRead();
-void flash();
-int buttonReg2index(uint8_t button);
+void eepromWrite();
+void runLightsDown();
+uint8_t buttonReg2index(uint8_t button);
 void setup();
 void loop();
 #line 1 "src/trigger_riot.ino"
@@ -19,7 +20,7 @@ void loop();
 #define OUT_PORT PORTD
 #define ALL_OFF (OUT_PORT &= ~(B11111100))
 #define ALL_ON (OUT_PORT |= (B11111100))
-#define NO_BUTTON_PRESSED 0b00001111
+#define NO_BUTTON_PRESSED B00001111
 #define ADC_BITS 10
 #define ADC_DIV_SHIFT 6
 #define ADC_PROB_SHIFT 3
@@ -29,6 +30,9 @@ void loop();
 #define SW2 B00001101
 #define SW3 B00001011
 #define SW4 B00000111
+#define D_CLICK_TIME 250
+#define DIV_EEPROM_ADDR_ST 0;
+#define PROB_EEPROM_ADDR_OFFSET 5
 
 #define max(a,b) (((a)>(b))?(a):(b))
 #define min(a,b) (((a)<(b))?(a):(b))
@@ -36,28 +40,28 @@ void loop();
 #define IN_RANGE(min,max,val) (((val) < (min)) ? (min) : (((val) > (max)) ? (max) : (val)))
 
 
-uint16_t divtable[NUM_DIVS] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 16, 32, 64, 128, 128};
+uint8_t divtable[NUM_DIVS] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 16, 32, 64, 128, 128};
 
-long prevTime;
-long currTime;
+unsigned long prevTime;
+unsigned long currTime;
 
 //Random variables
 uint8_t randNum;
-uint8_t randComp[NUM_OUTPUTS] = {0, 0, 0, 0};
+uint8_t prob[NUM_OUTPUTS] = {0, 0, 0, 0};
 
 //Output pins
 uint8_t clockOut = 2;
-uint8_t o1 = 3;
-uint8_t o2 = 4;
-uint8_t o3 = 5;
-uint8_t o4 = 6;
+const uint8_t o1 = 3;
+const uint8_t o2 = 4;
+const uint8_t o3 = 5;
+const uint8_t o4 = 6;
 uint8_t out[NUM_OUTPUTS] = {o1, o2, o3, o4};
 
 //Select switches pins
-uint8_t s1 = 0;
-uint8_t s2 = 1;
-uint8_t s3 = 2;
-uint8_t s4 = 3;
+const uint8_t s1 = 0;
+const uint8_t s2 = 1;
+const uint8_t s3 = 2;
+const uint8_t s4 = 3;
 
 //Array holding division for each output
 uint8_t divs[NUM_OUTPUTS] = {1, 1, 1, 1};
@@ -66,53 +70,68 @@ uint8_t outState;
 
 uint8_t trigTime = 20;
 uint8_t clock;
-long counter;
+unsigned long counter;
+unsigned long dClickInt1;
+unsigned long dClickInt2;
 
 uint16_t clockAdc;
 uint8_t scaledDivAdc;
 uint8_t scaledProbAdc;
 
-uint8_t tempDiv;
-uint8_t tempComp;
+uint8_t compareDiv;
+uint8_t compareProb;
 uint8_t divWait;
-uint8_t compWait;
+uint8_t probWait;
 uint8_t buttonPressed;
 
 uint8_t i;
 uint8_t eepromReadFlag;
+uint8_t check_buttons;
 uint8_t button;
+uint8_t b2i;
+uint8_t dClickDetected;
 
 int checkProb(uint8_t num, uint8_t comp)
 {
-	return (num >= comp) ? 1:0;
+	return (num >= comp) ? 1 : 0;
 }
 
 void eepromRead()
 {
 	for(i=0; i<NUM_OUTPUTS; i++){
 		divs[i] = EEPROM.read(i);
+		prob[i] = EEPROM.read(i+PROB_EEPROM_ADDR_OFFSET);
+	}
+	runLightsDown();
+}
+
+void eepromWrite()
+{
+	for(i=0; i<NUM_OUTPUTS; i++){
+		EEPROM.write(i, divs[i]);
+		EEPROM.write(i + PROB_EEPROM_ADDR_OFFSET, prob[i]);
+	}
+	runLightsDown();
+}
+
+void runLightsDown()
+{
+	for(i=0; i<NUM_OUTPUTS; i++){
+		OUT_PORT |= (1<<out[i]);
+		delay(100);
 	}
 	for(i=0; i<NUM_OUTPUTS; i++){
-		randComp[i] = EEPROM.read(i+5);
+		OUT_PORT &= ~(1<<out[i]);
+		delay(100);
 	}
 }
 
-void flash()
-{
-	OUT_PORT = ALL_ON;
-	delay(500);
-	OUT_PORT = ALL_OFF;
-	delay(500);
-	OUT_PORT = ALL_ON;
-	delay(500);
-	OUT_PORT = ALL_OFF;
-}
 
-int buttonReg2index(uint8_t button)
+uint8_t buttonReg2index(uint8_t button)
 {
 	uint8_t index;
 
-	if (!divWait){
+	if (1){
 		switch(button){
 			case SW1:
 				index = 0;
@@ -161,12 +180,6 @@ void setup()
 
 void loop()
 {
-
-	if(!eepromReadFlag){
-		eepromRead();
-		eepromReadFlag = 1;
-	}
-
 	currTime = millis();
 	clockAdc = ((1<<10) - analogRead(2));
 
@@ -175,85 +188,69 @@ void loop()
 
 	//Monitor select buttons. If pressed, store adc val
 	if(!buttonPressed){
-		tempDiv = scaledDivAdc;
-		tempComp = scaledProbAdc;
+		compareDiv = scaledDivAdc;
+		compareProb = scaledProbAdc;
 	}
 
 	//If stored adc val != current -> Pot has changed, ok to write val to array.
-	if(tempDiv != scaledDivAdc){
+	//Move to buttonPress If?
+	if(compareDiv != scaledDivAdc){
 		divWait = 0;
 	}
 
-	if(tempComp != scaledProbAdc){
-		compWait = 0;
+	if(compareProb != scaledProbAdc){
+		probWait = 0;
 	}
 
 	//trigTime = IN_RANGE(5, (clockAdc - 10), analogRead(3));
 
-	//Monitor all buttons, if any is pressed, make adc write procedure wait for pot movement.
+/*******************************************************************
+*Monitor button read register. If any b is pressed, indicate with 
+*buttonPress and set wait flags. These will keep the current settings
+*for the selected outport until a pot is changed.
+*Convert value of register to index.
+*******************************************************************/
+	check_buttons = (SW_READ_PORT & B00001111);
 
-	if((button = (SW_READ_PORT & 0b00001111)) != NO_BUTTON_PRESSED){
-		if(!buttonPressed){
-			buttonPressed = 1;
-			divWait = 1;
-			compWait = 1;
+	if(check_buttons != NO_BUTTON_PRESSED){
+
+		b2i = buttonReg2index(check_buttons);
+
+		dClickInt1 = millis();
+		if(((dClickInt1 - dClickInt2) < D_CLICK_TIME) && !dClickDetected){
+			dClickDetected = 1;
+			divs[b2i] = 1;
+			prob[b2i] = 0;
 		}
 
-		if (!divWait){
-			divs[buttonReg2index(button)] = scaledDivAdc;
-		}
-
-		if (!compWait){
-			randComp[buttonReg2index(button)] = scaledProbAdc;
+		else{
+			if(!buttonPressed){
+				buttonPressed = 1;
+				divWait = 1;
+				probWait = 1;
+			}
+			if(!divWait){
+				divs[b2i] = scaledDivAdc;
+			}
+			if(!probWait){
+				prob[b2i] = scaledProbAdc;
+			}
 		}
 	}
-	
-	else
+	else{
+		dClickInt2 = dClickInt1;
 		buttonPressed = 0;
-
-	/*//Check buttons and change if pot is moved
-	if(!(SW_READ_PORT & (1<<s1))){
-		if(!divWait)
-			divs[0] = scaledDivAdc;
-		
-		if(!compWait)
-			randComp[0] = scaledProbAdc;
+		dClickDetected = 0;
 	}
-
-	else if(!(SW_READ_PORT & (1<<s2))){
-		if(!divWait)
-			divs[1] = scaledDivAdc;
-		
-		if(!compWait)
-			randComp[1] = scaledProbAdc;
-	}
-
-	else if(!(SW_READ_PORT & (1<<s3))){
-		if(!divWait)
-			divs[2] = scaledDivAdc;
-		
-		if(!compWait)
-			randComp[2] = scaledProbAdc;
-	}
-
-	else if(!(SW_READ_PORT & (1<<s4))){
-		if(!divWait)
-			divs[3] = scaledDivAdc;
-		
-		if(!compWait)
-			randComp[3] = scaledProbAdc;;
-	}*/
 
 
 	//Write to eeprom if button 3&4 are pressed.
 	if((!(SW_READ_PORT & (1<<s4))) && (!(SW_READ_PORT & (1<<s3)))){
-		flash();
-		for(i=0; i<NUM_OUTPUTS; i++){
-			EEPROM.write(i, divs[i]);
-		}
-		for(i=0; i<NUM_OUTPUTS; i++){
-			EEPROM.write(i + 5, randComp[i]);
-		}
+		eepromWrite();
+	}
+
+	if((!(SW_READ_PORT & (1<<s1))) && (!(SW_READ_PORT & (1<<s2)))){
+		eepromRead();
 	}
 
 	//Master clock
@@ -273,7 +270,7 @@ void loop()
 
 		for(i=0; i<NUM_OUTPUTS; i++)
 		{
-			if(((counter % divs[i]) == 0) && checkProb(randNum, randComp[i])){
+			if(((counter % divs[i]) == 0) && checkProb(randNum, prob[i])){
 				OUT_PORT |= (1<<out[i]);
 			}
 		}
